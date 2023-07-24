0e595a95457c9ce0b2f1fc47584a2da9213fdf88
bharathril <Bharat12.Kumar@ril.com>
2023-07-24 10:56:09 +0530

    for testing

---
+++ tag.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# CURRENT_BRANCH=$(git symbolic-ref --short HEAD)
+v=1
+v=v+1
+echo $v
+# Specify the tag format you want to use
+# TAG_FORMAT="$CURRENT_BRANCH-$v"
+
+# # Create a tag with the specified format
+# TAG_NAME=$(date +$TAG_FORMAT)
+# git tag $TAG_NAME
+
