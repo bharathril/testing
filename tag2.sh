61c285a91666508da59cd6a0b3aa7b4de0d872b8
bharathril <Bharat12.Kumar@ril.com>
2023-07-24 12:54:42 +0530

    changes made

---
+++ tag2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+CURRENT_BRANCH=$(git symbolic-ref --short HEAD)
+TAG_FORMAT="$CURRENT_BRANCH-%Y%m%d-V1.0"
+varone=$(git describe --tags | cut -d '.' -f 1)
+vartwo=$(git describe --tags | cut -d '.' -f 2)
+varthree=$((vartwo + 1))
+echo "$varthree"
+newformat="$varone.$varthree"
+
+git tag $newformat
